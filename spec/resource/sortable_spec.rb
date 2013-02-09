f5872f831af9a5699c5d53241f32048f6f39d411
Gavin Joyce <gavinjoyce@gmail.com>
2013-02-09 13:01:44 +0000

    added a couple more sortable specs

--- spec/resource/sortable_spec.rb
+++ spec/resource/sortable_spec.rb
@@ -10,6 +10,33 @@ describe RestPack::Resource do
           end.to raise_error(RestPack::Resource::InvalidSortBy)
         end
       end
+      
+      context "with valid options" do
+        before(:each) do
+          30.times { FactoryGirl.create(:artist) }
+        end
+        
+        it "should default to ascending" do
+          result = Artist.resource_paged_resource(:sort_by => :id)
+          result[:total].should == Artist.count
+          result[:artists].first[:id].should == 1
+          result[:artists].last[:id].should == 10
+        end
+
+        it "should allow sort_direction of descending" do
+          result = Artist.resource_paged_resource(:sort_by => :id, :sort_direction => :descending)
+          result[:total].should == Artist.count
+          result[:artists].first[:id].should == 30
+          result[:artists].last[:id].should == 21
+        end
+
+        it "should allow sort_by and sort_direction to be string or symbol" do
+          result = Artist.resource_paged_resource(:sort_by => 'id', :sort_direction => 'descending')
+          result[:total].should == Artist.count
+          result = Artist.resource_paged_resource(:sort_by => :id, :sort_direction => :descending)
+          result[:total].should == Artist.count
+        end
+      end
     end
   end
 end
